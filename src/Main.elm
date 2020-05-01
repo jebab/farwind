64027b4206ac6f5ab4e82cd0d3b62c9ac52e8cb2
jabab <>
2020-05-01 22:20:44 +1100

    Maj images

--- src/Main.elm
+++ src/Main.elm
@@ -47,31 +47,47 @@ update message model =
 
 view : Model -> Html Msg
 view _ =
-    Html.div
+    Html.main_
         []
-        [ Html.section [ Attribute.class "section" ]
-            [ Html.div [ Attribute.class "container has-text-centered" ]
-                [ Html.img
-                    [ Attribute.src "/images/tilded-logo.webp"
-                    , Attribute.alt "Farwind : We convert far-offshore wind energy into clean renewable fuel"
-                    , Attribute.height 56
-                    ]
-                    []
-                ]
-            ]
-        , Html.section [ Attribute.class "section" ]
-            [ Html.div [ Attribute.class "container has-text-centered" ]
-                [ Html.img [ Attribute.src "/images/boat.webp", Attribute.alt "80m Autonomous Catamaran" ] []
-                ]
-            ]
-        , Html.div
-            [ Attribute.class "hero" ]
-            [ Html.div [ Attribute.class "container has-text-centered" ]
-                [ Html.p [ Attribute.class "content" ]
-                    [ Html.h3 [ Attribute.class "subtitle " ] [ Html.text "We convert far-offshore wind energy into clean renewable fuel." ]
-                    , Html.a [ Attribute.class "button", Attribute.href "mailto:contact@farwind-energy.com" ] [ Html.text "Contact" ]
+        [ Html.section [ Attribute.class "hero is-fullheight" ]
+            [ Html.div [ Attribute.class "hero-head" ]
+                [ Html.div [ Attribute.class "container has-text-centered " ]
+                    [ Html.p [] [ Html.h1 [ Attribute.class "has-text-primary is-logo is-hidden-touch is-family-secondary" ] [ Html.text "Farwind" ] ]
+                    , Html.p [] [ Html.h1 [ Attribute.class "has-text-primary is-mobile-logo is-hidden-desktop is-family-secondary" ] [ Html.text "Farwind" ] ]
                     ]
                 ]
+            , Html.div [ Attribute.class "hero-body " ]
+                [ Html.div [ Attribute.class "container has-text-centered " ]
+                    [ Html.section [ Attribute.class "columns" ]
+                        [ Html.div
+                            [ Attribute.class
+                                "column is-half-desktop is-offset-one-quarter-desktop is-three-fifths-tablet is-offset-one-fifth-tablet"
+                            ]
+                            [ Html.figure [ Attribute.class "image is-3by2" ]
+                                [ Html.img
+                                    [ Attribute.attribute "sizes" "(max-width: 1520px) 100vw, 1520px"
+                                    , Attribute.attribute "srcset" <|
+                                        String.join ","
+                                            [ "/images/boat_utgpat_c_scale,w_190.webp 190w,"
+                                            , "/images/boat_utgpat_c_scale,w_762.webp 762w,"
+                                            , "/images/boat_utgpat_c_scale,w_1162.webp 1162w,"
+                                            , "/images/boat_utgpat_c_scale,w_1520.webp 1520w,"
+                                            ]
+                                    , Attribute.src "/images/boat_utgpat_c_scale,w_1520.webp"
+                                    , Attribute.alt "An eighty meters autonomous catamaran"
+                                    ]
+                                    []
+                                ]
+                            ]
+                        ]
+                    , Html.p [ Attribute.class "content " ]
+                        [ Html.h3 [ Attribute.class "subtitle" ] [ Html.text "We convert far-offshore wind energy into clean renewable fuel." ]
+                        , Html.a [ Attribute.class "button  ", Attribute.href "mailto:contact@farwind-energy.com" ] [ Html.text "Contact" ]
+                        ]
+                    ]
+                ]
+            , Html.div [ Attribute.class "hero-foot " ]
+                []
             ]
         ]
 
